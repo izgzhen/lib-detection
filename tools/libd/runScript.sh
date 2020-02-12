cddddc52c01cde8699aa2ff96f1d62145cc018b6
yanglushu <871467263@qq.com>
2020-02-11 21:24:30 -0800

    add runScript.sh and manuallyApproach. The manuallyApproach folder is needed, so that we can have precomputed libraries list

--- tools/libd/runScript.sh
+++ tools/libd/runScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for apkSample in `ls $1`; do
-    echo "Running analysis on $1/$apkSample"
-    python libd_v_0.0.1.py $1/$apkSample decompose/ manuallyapproach/whitelist
+    echo "Running analysis on $1/$apkSample" 
+    python libd_v_0.0.1.py $1/$apkSample decompose/ manuallyapproach/whitelist 
 done
