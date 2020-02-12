126638717d868d8acae34e98d265610e1de4eb43
Zhen Zhang <izgzhen@gmail.com>
2020-02-12 08:31:23 -0800

    Redirect output inside container to output

--- tools/libd/runScript.sh
+++ tools/libd/runScript.sh
@@ -2,5 +2,5 @@
 
 for apkSample in `ls $1`; do
     echo "Running analysis on $1/$apkSample"
-    python libd_v_0.0.1.py $1/$apkSample decompose/ manuallyapproach/whitelist
+    python libd_v_0.0.1.py $1/$apkSample decompose/ manuallyapproach/whitelist > /mnt/output/$apkSample.txt
 done
--- tools/libradar/runScript.sh
+++ tools/libradar/runScript.sh
@@ -2,5 +2,5 @@
 
 for apkSample in `ls $1`; do
     echo "Running analysis on $1/$apkSample" 
-    python LiteRadar/LiteRadar/literadar.py $1/$apkSample
+    python LiteRadar/LiteRadar/literadar.py $1/$apkSample > /mnt/output/$apkSample.txt
 done
