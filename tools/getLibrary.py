66146256f0342a42e2d4729e8e728a026dc7dcae
yanglushu <871467263@qq.com>
2020-02-18 21:28:40 -0800

    resolve all comments

--- tools/getLibrary.py
+++ tools/getLibrary.py
@@ -1,7 +1,9 @@
 import os, subprocess
 import sys
+import glob
+from collections import Counter
 
-def getLibraryLibd(path_to_apk, word_to_frequency):
+def get_library_libd(path_to_apk, total_libraries):
     res = set()
     with open(path_to_apk, 'r') as file:
         line = file.readline()
@@ -11,15 +13,18 @@ def getLibraryLibd(path_to_apk, word_to_frequency):
         line = file.readline()
         while line:
             # get rid of the backslash at the end of the package name
-            package = line.strip()[:-1]
+            line = line.strip()
+            package = line
+            if line[-1] == '/':
+                package = package[:-1]
             res.add(package)
-            word_to_frequency[package] = word_to_frequency.get(package, 0) + 1
+            total_libraries.append(package)
             line = file.readline()
     file.close()
     return res
 
 
-def getLibraryLibRadar(path_to_apk, word_to_frequency):
+def get_library_libRadar(path_to_apk, total_libraries):
     res = set()
     with open(path_to_apk, 'r') as file:
         line = file.readline()
@@ -30,51 +35,57 @@ def getLibraryLibRadar(path_to_apk, word_to_frequency):
                 # get rid of double quotes around package
                 package = line.split(": \"")[1][:-2]
                 res.add(package)
-                word_to_frequency[package] = word_to_frequency.get(package, 0) + 1
+                total_libraries.append(package)
             line = file.readline()
     file.close()
     return res
 
 
-def findIntersection(libdRes, libradarRes, word_to_frequency):
-    """ check if they are substring of each other """
-    res = list(libdRes & libradarRes)
+def findIntersection(apk_name, libd_res, libradar_res, word_to_frequency):
+    """find the intersection of the parsed libraries from libd and libradar"""
 
-    libdRes_leftOut = [value for value in libdRes if value not in res] 
-    libradarRes_leftOut = [value for value in libradarRes if value not in res] 
+    res = list(libd_res & libradar_res)
 
+    libd_res_leftOut = [value for value in libd_res if value not in res] 
+    libradar_res_leftOut = [value for value in libradar_res if value not in res] 
+    print("Finding the overlapping libraries on " + apk_name)
     print("The intersections are: ")
     print(res)
     print("The libraries that are not in the intersection for libd are: ")
-    for library in libdRes_leftOut:
-        print(library + ', the relative frequency is ' + str(word_to_frequency[library]))
+    for library in libd_res_leftOut:
+        print(library + ', the frequency is ' + str(word_to_frequency[library]))
     print("\n")
-    print("The libraries that are not in the intersection for libdradarRes are: ")
-    for library in libradarRes_leftOut:
-        print(library + ', the relative frequency is ' + str(word_to_frequency[library]))
+    print("The libraries that are not in the intersection for libradar are: ")
+    for library in libradar_res_leftOut:
+        print(library + ', the frequency is ' + str(word_to_frequency[library]))
     print("\n")
-    return res
 
 
 def main() :
     """ usage: python getLibrary.py output_folder_libd output_folder_libradar"""
-    path_libd = str(sys.argv[1])
-    path_libradar = str(sys.argv[2])
-    files_libd = subprocess.Popen(["ls", path_libd], stdout=subprocess.PIPE)
+    path_libd = sys.argv[1]
+    path_libradar = sys.argv[2]
+    file_names_libd = glob.glob(path_libd + "*.txt")
+    file_names_libradar = glob.glob(path_libradar + "*.txt")
+    
+    # find the common apk files between two result folders
+    file_names_libd = [i.split(path_libd)[1] for i in file_names_libd]
+    file_names_libradar = [i.split(path_libradar)[1] for i in file_names_libradar]
+    common_file_names = [value for value in file_names_libd if value in file_names_libradar] 
 
-    file_names_libd = files_libd.stdout
-    word_to_frequency = dict()
-
-    apk_to_res = dict()
-    for apk_sample in file_names_libd:
-        libdRes = getLibraryLibd(path_libd + apk_sample.strip(), word_to_frequency)
-        libradarRes = getLibraryLibRadar(path_libradar + apk_sample.strip(), word_to_frequency)
-        apk_to_res[apk_sample] = (libdRes, libradarRes)
+    # parsed the libraries, count the library and its frequency in the entire dataset
+    total_libraries = []
+    apk_to_parsed_libraries = dict()
+    for apk_sample in common_file_names:
+        libd_res = get_library_libd(path_libd + apk_sample.strip(), total_libraries)
+        libradar_res = get_library_libRadar(path_libradar + apk_sample.strip(), total_libraries)
+        apk_to_parsed_libraries[apk_sample] = (libd_res, libradar_res)
+    library_to_frequency = Counter(total_libraries)
     
     # find intersection
-    for apk_sample in apk_to_res:
-        libd_libradar = apk_to_res[apk_sample]
-        findIntersection(libd_libradar[0], libd_libradar[1], word_to_frequency)
+    for apk_name, parsed_two_libraries in apk_to_parsed_libraries.items():
+        libd_parsed_libraries, libradar_parsed_libraries = parsed_two_libraries
+        findIntersection(apk_name, libd_parsed_libraries, libradar_parsed_libraries, library_to_frequency)
 
 
 if __name__ == "__main__":
